bc66c15bb111956f86e957f0b847028076361479
Emily Irvine <emilymirvine@gmail.com>
2019-08-05 14:25:48 -0400

    Setting up code for R187

---
+++ info/r187_exp.py
@@ -0,0 +1,42 @@
+import os
+import numpy as np
+import nept
+
+rat_id = 'R187'
+session_id = 'R187_exp'
+date = '2019-08-xx'
+session = 'R187-'+date
+
+location = 'RR1'
+
+species = 'rat'
+behavior = 'experience 4-arm maze'
+experimenter = 'Emily Irvine'
+
+event_filename = date + '-Events.nev'
+event_labels = dict(north_feeder='TTL Output on AcqSystem1_0 board 0 port 2 value (0x0010).',
+                    not_north_feeder='NOT firing feeder NorthFeeder',
+                    north_pb='TTL Input on AcqSystem1_0 board 0 port 1 value (0x0010).',
+                    east_feeder='TTL Output on AcqSystem1_0 board 0 port 2 value (0x0020).',
+                    not_east_feeder='NOT firing feeder EastFeeder',
+                    east_pb='TTL Input on AcqSystem1_0 board 0 port 1 value (0x0020).',
+                    south_feeder='TTL Output on AcqSystem1_0 board 0 port 2 value (0x0040).',
+                    not_south_feeder='NOT firing feeder SouthFeeder',
+                    south_pb='TTL Input on AcqSystem1_0 board 0 port 1 value (0x0040).',
+                    west_feeder='TTL Output on AcqSystem1_0 board 0 port 2 value (0x0080).',
+                    not_west_feeder='NOT firing feeder WestFeeder',
+                    west_pb='TTL Input on AcqSystem1_0 board 0 port 1 value (0x0080).',
+                    trial_start='trial start',
+                    trial_end='trial end',
+                    unknown='TTL Input on AcqSystem1_0 board 0 port 1 value (0x00C0).',
+                    approach_state='entering approach state',
+                    return_state='entering return state')
+
+position_filename = 'VT1.nvt'
+pxl_to_cm = dict(x=3.13, y=2.73)
+
+lfp_swr_filename = 'CSC11a.ncs'
+
+binsize = 12
+xedges = np.arange(14, 715+binsize, binsize)
+yedges = np.arange(0, 479+binsize, binsize)
---
+++ info/r187_tmaze.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+import nept
+
+rat_id = 'R187'
+
+species = 'rat'
+behavior = 't-maze'
+experimenter = 'Emily Irvine'
+
+sessions = ["2019-07-01", "2019-07-02", "2019-07-03", "2019-07-04", "2019-07-05", "2019-07-06", "2019-07-07",
+			"2019-07-08", "2019-07-09", "2019-07-10", "2019-07-11", "2019-07-12", "2019-07-13", "2019-07-14",
+			"2019-07-15", "2019-07-16", "2019-07-17", "2019-07-18", "2019-07-30", "2019-07-31", "2019-08-01",
+            "2019-08-02", "2019-08-03", "2019-08-04", "2019-08-05"]
+
+n_sessions_presurgery = 18
+
+event_labels = dict(north_feeder='TTL Output on AcqSystem1_0 board 0 port 0 value (0x0010).',
+                    not_north_feeder='NOT firing feeder NorthFeeder',
+                    north_pb='TTL Input on AcqSystem1_0 board 0 port 1 value (0x0010).',
+                    east_feeder='TTL Output on AcqSystem1_0 board 0 port 0 value (0x0020).',
+                    not_east_feeder='NOT firing feeder EastFeeder',
+                    east_pb='TTL Input on AcqSystem1_0 board 0 port 1 value (0x0020).',
+                    south_feeder='TTL Output on AcqSystem1_0 board 0 port 0 value (0x0040).',
+                    not_south_feeder='NOT firing feeder SouthFeeder',
+                    south_pb='TTL Input on AcqSystem1_0 board 0 port 1 value (0x0040).',
+                    trial_start='trial start',
+                    trial_end='trial end')
