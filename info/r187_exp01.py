27dd2eac44d902a97cd87becb0d538458ec78600
emirvine <emilymirvine@gmail.com>
2019-08-10 18:53:46 -0400

    Loading recording data from RR1

--- info/r187_exp01.py
+++ info/r187_exp01.py
@@ -3,8 +3,8 @@ import numpy as np
 import nept
 
 rat_id = 'R187'
-session_id = 'R187_exp'
-date = '2019-08-08'
+session_id = 'R187_exp01'
+date = '2019-08-09'
 session = 'R187-'+date
 
 location = 'RR1'
@@ -15,8 +15,8 @@ experimenter = 'Emily Irvine'
 
 event_filename = date + '-Events.nev'
 event_labels = dict(recording_start='Starting Recording',
-					recording_stop='Stopping Recording',
-					north_feeder='TTL Output on AcqSystem1_0 board 0 port 2 value (0x0010).',
+				recording_stop='Stopping Recording',
+				north_feeder='TTL Output on AcqSystem1_0 board 0 port 2 value (0x0010).',
                     not_north_feeder='NOT firing feeder NorthFeeder',
                     north_pb='TTL Input on AcqSystem1_0 board 0 port 1 value (0x0010).',
                     east_feeder='TTL Output on AcqSystem1_0 board 0 port 2 value (0x0020).',
@@ -46,6 +46,11 @@ xedges = np.arange(14, 715+binsize, binsize)
 yedges = np.arange(0, 479+binsize, binsize)
 
 task_times = dict()
-task_times['rest1'] = nept.Epoch([479.94], [611.0])
-task_times['run1'] = nept.Epoch([714.74], [1313.55])
-task_times['rest2'] = nept.Epoch([1375.58], [1506.21])
+task_times['rest1'] = nept.Epoch([336.76], [1261.41])
+task_times['run1'] = nept.Epoch([1311.69], [3003.76])
+task_times['rest2'] = nept.Epoch([3029.79], [3933.87])
+task_times['run2'] = nept.Epoch([4013.66], [5359.62])
+task_times['rest3'] = nept.Epoch([5406.63], [6367.69])
+
+probe_choice = ['east', 'south', 'west', 'north',
+				'west', 'north', 'east', 'south']
