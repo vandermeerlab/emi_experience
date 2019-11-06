3b889fb11a68b125fd480fe571beb0784e815b8a
Emily Irvine <emilymirvine@gmail.com>
2019-11-06 12:12:33 -0500

    Quantifying n_swrs in 15 min rest for all R191 and R192 sessions

--- info/__init__.py
+++ info/__init__.py
@@ -1,1 +1,13 @@
 import info.r181_tmaze
+
+import info.r191_exp01
+import info.r191_exp02
+import info.r191_exp03
+import info.r191_exp04
+import info.r191_exp05
+
+import info.r192_exp01
+import info.r192_exp02
+import info.r192_exp03
+import info.r192_exp04
+import info.r192_exp05
---
+++ run.py
@@ -0,0 +1,9 @@
+import os
+import info
+
+thisdir = os.path.dirname(os.path.realpath(__file__))
+
+analysis_infos = [
+        info.r191_exp01, info.r191_exp02, info.r191_exp03, info.r191_exp04, info.r191_exp05,
+        info.r192_exp01, info.r192_exp02, info.r192_exp03, info.r192_exp04, info.r192_exp05
+        ]
